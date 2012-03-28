5b18e85b838ebff7a3481ddcd7eaf9afad65c3df
Tony Seing <tony.seing@gmail.com>
2012-03-28 19:39:40 -0400

    possible ie fix--referrer for fb share set in javascript instead of ruby

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #production
-  provider :facebook, '363753320314028', 'da1e0091664130d7768edf2cfe442c92', {:scope => 'publish_stream'}
+  #provider :facebook, '363753320314028', 'da1e0091664130d7768edf2cfe442c92', {:scope => 'publish_stream'}
+  
+  #development
+  provider :facebook, '105799622886522', 'efb69ce3022d037ded89a2472e511472', {:scope => 'publish_stream'}
 
  
 end
